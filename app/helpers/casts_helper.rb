a955112723d2ec832e0ca960820ff9afcdee0e76
Halsey <lyu302@gmail.com>
2013-01-27 23:35:33 +0800

    add upload flash

--- app/helpers/casts_helper.rb
+++ app/helpers/casts_helper.rb
@@ -1,2 +1,14 @@
+require 'net/http'
+require File.expand_path("../../../lib/crypt-xxtea/xxtea", __FILE__)
 module CastsHelper
+
+  def get_result
+    url, sn, key = "http://v.ku6vms.com/v3/api", "27bf6226213cf288dfbf62ffc02bad4f", "b6914f0cdacde53bfe6751ac05880111"
+    time = Time.now.to_i
+    crypt = XXTEA.encrypt(key, time.to_s) 
+    url = URI.parse(url) unless url.is_a?(URI) 
+    conn = Net::HTTP.new(url.host, url.port)
+    rsp = conn.get("/v3/api?method=GetFlashUploadParam&sn=#{sn}&posttime=#{time}&token=#{crypt}", {})
+    rsp.body
+  end
 end
