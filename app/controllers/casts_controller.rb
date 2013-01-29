bdfbf788b14d29e4323a8a843ae5cd8198fb9630
Three-letter <chx2103@gmail.com>
2013-01-29 18:30:04 +0800

    add upload

--- app/controllers/casts_controller.rb
+++ app/controllers/casts_controller.rb
@@ -16,18 +16,6 @@ class CastsController < ApplicationController
 
   def create
     @cast = Cast.new(params[:cast].merge(user_id: current_user.id))
-    
-    Qiniu::RS.upload_file :uptoken    => get_upload_token,
-                          :file       => params[:cast][:video].tempfile.path,
-                          :bucket     => "ppst",
-                          :key        => "#{current_user.id}-#{params[:cast][:video].original_filename}",
-                          :callback_params => {bucket: "<BucketName>", key: "<FileUniqKey>", uid: "<customer>"}
-    #if @cast.save
-    #  flash[:cast] = "视频上传成功"
-    #  redirect_to @cast
-    #else
-    #  render :new
-    #end
   end
 
   def edit
@@ -37,12 +25,22 @@ class CastsController < ApplicationController
     @cast = Cast.find_by_id(params[:id])
   end
 
+
+  def upload
+    unless params[:file].blank?
+      result = Qiniu::RS.upload_file :uptoken    => get_upload_token,
+                                     :file       => params[:upload].tempfile.path,
+                                     :bucket     => "ppst",
+                                     :key        => "#{current_user.id}-#{params[:upload].original_filename}"
+    end
+  end
+
   private
 
   def get_upload_token
     Qiniu::RS.generate_upload_token :scope                =>  "ppst",
                                     :expires_in           =>  60 * 30,
-                                    :callback_url         =>  "http://localhost:3000",
+                                    :callback_url         =>  "",
                                     :callback_body_type   =>  "application/json",
                                     :customer             =>  current_user.id.to_s,
                                     :escape               =>  1,
